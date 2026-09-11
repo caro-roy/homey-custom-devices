1422b55d13a563e269f6d290db656b5b4efe9f9b
Caroline Roy <caroline.roy.dev@gmail.com>
2026-09-11 14:53:05 +0200

    Tile + driver

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const Homey = require('homey')
+
+module.exports = class MyApp extends Homey.App {
+  /**
+   * onInit is called when the app is initialized.
+   */
+  async onInit() {
+    this.log('Waou')
+  }
+}
---
+++ drivers/small-led-candles/device.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const Homey = require('homey')
+
+module.exports = class SmallLEDCandles extends Homey.Device {
+  /**
+   * onInit is called when the device is initialized.
+   */
+  async onInit() {
+    this.signal = this.homey.rf.getSignalInfrared('small-led-candles')
+
+    this.registerCapabilityListener('onoff', async value => {
+      await this.sendCmd(value ? 'ON' : 'OFF')
+    })
+
+    this.registerCapabilityListener('button.lum_down', async () => {
+      await this.sendCmd('LUM_DOWN')
+    })
+
+    this.registerCapabilityListener('button.lum_up', async () => {
+      await this.sendCmd('LUM_UP')
+    })
+
+    this.registerCapabilityListener('light-mode', async value => {
+      await this.sendCmd(`MODE_${value}`.toUpperCase())
+    })
+  }
+
+  async sendCmd(cmd) {
+    await this.signal.cmd(cmd)
+  }
+}
